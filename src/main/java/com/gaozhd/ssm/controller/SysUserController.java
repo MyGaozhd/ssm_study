2c3ee96d5871d335a07c50d08046509bdd92f9ed
高志东 <gaozhdf@yonyou.com>
2018-06-19 15:45:47 +0800

    测试非注解的处理器映射器，处理器适配器

--- src/main/java/com/gaozhd/ssm/controller/SysUserController.java
+++ src/main/java/com/gaozhd/ssm/controller/SysUserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @Controller
 @RequestMapping("/user")
@@ -18,7 +19,7 @@ public class SysUserController {
             RequestMethod.POST })
     @ResponseBody
     public SysUser getOneUser() throws Exception {
-
+        DispatcherServlet a ;
         return userService.findSysUserByID(1);
     }
 }

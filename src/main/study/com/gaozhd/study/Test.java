91fd1fa2c2c856abd79f6d3f39cfcf912c2aad35
高志东 <gaozhdf@yonyou.com>
2018-08-18 15:49:25 +0800

    增加zookeeper测试用例

---
+++ src/main/study/com/gaozhd/study/Test.java
@@ -0,0 +1,9 @@
+package com.gaozhd.study;
+
+import com.gaozhd.ssm.common.util.LogUtil;
+
+public class Test {
+    public static void main(String[] args) {
+        LogUtil.logJson("123");
+    }
+}
